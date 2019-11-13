2c698ed194f9ce55d65ff54cb640594978c343e2
Mikołaj Adamczyk <mikadamczyk@gmail.com>
2019-11-13 10:26:33 +0100

    fixup! Aligned tests with the changes

--- eZ/Publish/Core/MVC/Symfony/SiteAccess/Tests/RouterBaseTest.php
+++ eZ/Publish/Core/MVC/Symfony/SiteAccess/Tests/RouterBaseTest.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * File containing the eZ\Publish\Core\MVC\Symfony\SiteAccess\Tests\RouterURITextTest class.
- *
  * @copyright Copyright (C) eZ Systems AS. All rights reserved.
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
+declare(strict_types=1);
+
 namespace eZ\Publish\Core\MVC\Symfony\SiteAccess\Tests;
 
 use eZ\Publish\Core\MVC\Symfony\SiteAccess;
